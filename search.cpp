0451fd69f243b7231d82f4bd3330fbfa90eab96a
Zachary Hickey <zmhickey@Zacharys-MBP.local>
2020-09-02 17:58:53 -0500

    binary search

--- search.cpp
+++ search.cpp
@@ -6,6 +6,16 @@
   you need to find return
   if the item is not found return -1
 
+  Binary Search: 
+  given a sorted array list the length and an item to find
+  set min to 0 and max to the length of the array
+  while max <= min
+  set mid to the middle of min and max
+  check if array[mid] == what we want
+  if so return mid
+  else check if array[mid] < what we want
+  if so set min to mid + 1
+  else set max to max + 1
 *   
 */
 int LinearSearch(int numbers[], int length, int n) {
@@ -19,5 +29,16 @@ int LinearSearch(int numbers[], int length, int n) {
 
 int BinarySearch(int numbers[], int length, int n) {
     // TODO implement this function
-     return 1;
+  int min = 0,
+      max = length;
+  while ( max >= min){
+    int mid =  min + (max -min) / 2;
+    if ( numbers[mid] == n)
+      return mid;
+    if( numbers[mid] < n )
+      min = mid + 1;
+    else 
+      max = mid - 1;
+  };
+     return -1;
 }
