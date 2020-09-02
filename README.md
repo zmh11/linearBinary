9999375ec4a952f4aa15852e9c8820b1ff07da26
Zachary Hickey <zmhickey@Zacharys-MBP.local>
2020-09-02 17:22:14 -0500

    linear search

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # linearBinary
 homework2 linear and binary search
+
--- search.cpp
+++ search.cpp
@@ -1,10 +1,20 @@
 /****
-* PSEUDOCODE:
-*
+* PSEUDOCODE: linear search: 
+  given array list length of list and an item to find
+  loop through the array not exceding the array list
+  if the value in the list is the same as the item that 
+  you need to find return
+  if the item is not found return -1
+
+*   
 */
 int LinearSearch(int numbers[], int length, int n) {
     // TODO implement this function
-   return 1;
+  for ( int pos = 0; pos < length; pos++ ){
+    if ( numbers[pos] == n )
+      return pos;
+  }
+  return -1;
 }
 
 int BinarySearch(int numbers[], int length, int n) {
